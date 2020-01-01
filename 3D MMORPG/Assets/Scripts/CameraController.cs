ed50f50cc653ca9e040fb6bbfa1109e4e3b1f827
MrGabriel3600 <mgcolors3600@yahoo.com>
2020-01-01 14:08:42 -0500

    0.0.1 Build
    Added Camera follows player
    Added Camera Zoom

---
+++ 3D MMORPG/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    public float zoomSpeed = 4f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+
+    public float pitch = 2f;
+
+    private float currentZoom = 10f;
+
+    void Update ()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+    }
+    void LateUpdate()
+    {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up / pitch);
+    }
+}
