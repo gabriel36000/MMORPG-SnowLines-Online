4e5f4ac232cc69d1a056d23013f7f2837e664174
MrGabriel3600 <mgcolors3600@yahoo.com>
2020-04-16 17:47:35 -0400

    0.1.2 Build
    Added Health UI
    Added MP UI
    Added Level UI
    Added Map UI
    Added Skill Slot UI
    Added working Health System
    Added Resolution Opmtimizer

---
+++ 3D MMORPG/Assets/Scripts/HealthSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int playerMaxHealth;
+    public int playerCurrentHealth;
+    public Slider slider;
+
+    public 
+    void Start()
+    {
+        playerCurrentHealth = playerMaxHealth; // The Player will spawn with max health
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.maxValue = playerMaxHealth;
+        slider.value = playerCurrentHealth;
+        if (playerCurrentHealth < 0) {
+            gameObject.SetActive(false); // If the player dies, the game object will deactivated 
+        }
+    }
+    public void HurtPlayer(int damageToGive) {
+        playerCurrentHealth -= damageToGive;  //Players get's damage.
+    }
+
+    public void SetMaxHealth() {
+        playerCurrentHealth = playerMaxHealth; 
+    }
+}
