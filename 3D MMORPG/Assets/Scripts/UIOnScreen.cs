af8a11bd0bc1f9255dfb08f69d727b4ecdc1ba64
MrGabriel3600 <mgcolors3600@yahoo.com>
2020-04-22 23:51:53 -0400

    Build 0.1.5
    Added Combat System
    Added Health UI Text on Player
    Added Stats for players and enemies
    Added Enemy AI System

---
+++ 3D MMORPG/Assets/Scripts/UIOnScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIOnScreen : CharacterStats {
+    public Slider slider;
+    public Text HP_Current_And_Max;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.maxValue = maxHealth;
+        slider.value = currentHealth;
+
+        HP_Current_And_Max.text = "" + currentHealth + "/" + maxHealth;
+    }
+}
